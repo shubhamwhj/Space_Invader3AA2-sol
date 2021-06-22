1db5e919d715a229443d503a321182a3e23858f8
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-06-22 15:14:40 +0530

    Add files via upload

---
+++ SI_3AA2.py
@@ -0,0 +1,50 @@
+
+import pygame, sys
+
+pygame.init()
+clock=pygame.time.Clock()
+
+screen = pygame.display.set_mode((400,600))
+
+#creating objects of game
+player=pygame.Rect(200,500,30,30)
+playerSpeed=20
+enemy=pygame.Rect(70,50,40,40)
+enemyspeed= -2;
+
+while True:    
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.x -=playerSpeed
+            if event.key ==pygame.K_RIGHT:
+                player.x +=playerSpeed            
+            if event.key == pygame.K_UP:
+                player.y -=playerSpeed
+            if event.key ==pygame.K_DOWN:
+                player.y +=playerSpeed
+         
+    enemy.x= enemy.x + enemyspeed
+    
+    if enemy.x == 0:
+        enemyspeed=enemyspeed * -1
+        enemy.y= enemy.y + 20
+    
+    if enemy.x == 380:
+        enemyspeed=enemyspeed * -1
+        enemy.y= enemy.y + 20
+    
+           
+    pygame.draw.rect(screen,(123,200,100),enemy)
+    pygame.draw.rect(screen,(23,100,100),player)
+    
+    
+    pygame.display.update()
+    clock.tick(30)
+
